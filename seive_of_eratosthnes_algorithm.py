db0d977fa239804a0bc5fce460c42b77e8b48a45
Muse Semu <114074114+Muse-Semu@users.noreply.github.com>
2023-11-26 07:43:50 +0300

    Update seive_of_eratosthnes_algorithm.py

--- seive_of_eratosthnes_algorithm.py
+++ seive_of_eratosthnes_algorithm.py
@@ -1,5 +1,11 @@
 # this code implements based on Sieve_of_Eratosthenes
 
+# In mathematics, the sieve of Eratosthenes is an ancient algorithm for finding all prime numbers up to any given limit.
+# It does so by iteratively marking as composite (i.e., not prime) the multiples of each prime, starting with the first prime number, 2. 
+# The multiples of a given prime are generated as a sequence of numbers starting from that prime, with constant difference between them that is equal to that prime.
+# [1] This is the sieve's key distinction from using trial division to sequentially test each candidate number for divisibility by each prime.[2] 
+# Once all the multiples of each discovered prime have been marked as composites, the remaining unmarked numbers are primes.
+# https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 
 # This algorithm which is more efficiebt than usual programs due to large numbers (E.g print the sum of all prime below 2000000)
 
